3947b886711274e71e4ff1632080d10808d9f843
Michael Gao <michaelgao14@gmail.com>
2018-07-14 00:15:23 +0800

    fully integrated register design with vue-cli

---
+++ company-end/src/router.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import About from './views/About.vue';
+import Register from './views/Register.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+    },
+  ],
+});
