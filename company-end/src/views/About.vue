60b03a0bdcf433a86258fa13ddbd17e5261e4c86
John Matthew Flores <joflo.matres@gmail.com>
2018-07-14 18:37:18 +0800

    Added the css from rico and edited it a little

---
+++ company-end/src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+<div class='parent'>
+    <div class='child ' id='box-shadow'>
+        <div class='textStyle'>Now Serving</div>
+        <div class='prio-container'>
+            <p class='prioNumStyle'>32</p>
+        </div>
+        <div class='inputparent'>
+            <button class='buttonComplete large'>COMPLETE</button>
+            <button class='buttonSkip large'>CANCEL</button>
+        </div>
+    </div>
+</div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/sass/styles.scss";
+</style>
---
+++ company-end/src/views/Register.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div id="site-navbar">
+      <div class="container">
+        <div class="navbar-image">
+          <img class="navbar-image" src="../assets/img/logo.png" height="24px" alt="noline">
+        </div>
+        <input class="navbar-button border white" type="button" name="sign-in" value="Sign in">
+        <input class="navbar-button white" type="button" name="join-us" value="Join us">
+      </div>
+    </div>
+    <div class="img-container">
+      <img class="bg-img" src="../assets/img/bg.png">
+    </div>
+    <section id="register-feed">
+      <div class="form">
+        <div class="input-form">
+          <div class="main-description">Provide a better customer experience</div>
+          <small class="secondary-description">Just a few steps to get you started</small>
+          <form class="register-card">
+            <div class="floating-placeholder">
+              <label for="name">Company Name</label>
+              <input id="name" class="light" name="name" type="text"/>
+              <label for="email">Email Address</label>
+              <input id="email" class="light" name="name" type="text"/>
+              <label for="username">Username</label>
+              <input id="username" class="light" name="name" type="text"/>
+              <label for="password">Password</label>
+              <input id="password" class="light" name="name" type="text"/>
+              <label for="password1">Confirm Password</label>
+              <input id="password1" class="light" name="name" type="text"/>
+              <div class="submit-form">
+              <button class="blue large btn-submit">  SIGN IN </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/sass/styles.scss";
+</style>
